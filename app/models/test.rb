5b5d5771b9eab84fe54c64f0d21a75aa152ee4c4
Alex808r <fromxlam@gmail.com>
2021-11-30 17:01:48 +0300

    Add Sequel DSL to model test

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,6 +3,25 @@
 #   String :title, null: false
 #   Integer :level, default: 0
 # end
+
+# DSL Sequel
+
+# Create table 'tests' by irb
+
+# Simpler.application.db.create_table(:tests) do
+#   primary_key: id
+#   String :title, null: false
+#   Integer :level, default: 0
+# end
+
+# Create 'test' in table 'tests' by irb
+
+# require_relative 'config/environment'
+# Simpler.application
+# Simpler.application.db
+# Simpler.application.db[:tests].all
+# Simpler.application.db[:tests].insert(title: 'Java', level: 1)
+
 class Test < Sequel::Model
 
 end
